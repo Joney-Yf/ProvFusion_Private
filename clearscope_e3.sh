3ab386862fa5055d1e05eb2c4750b174bad120ed
Joney-Yf <562332218@qq.com>
2026-05-28 14:57:15 +0800

    Rewrite clearscope_e3.sh: fix data path and expand sweep params
    - Fix data_path: clearscope_e3.pt (4.9K, broken) -> clearscope_e3_merge_edge_data.pt (196M)
    - epochs=(5, 50, 200): full coverage across training lengths
    - lrf fixed to 0.001 (confirmed best from THEIA/CADETS sweeps)
    - lr expanded to (0.0015, 0.00015, 0.0001)
    - num_hiddens expanded to (64, 128, 256)
    - weight_decays: (0.01, 1e-3, 1e-4, 1e-5)
    - 972 total configs, single GPU, sequential
    - Add conda activation, fix GPU to CUDA_VISIBLE_DEVICES=1
    - Log to clearscope_e3_sweep.log

--- clearscope_e3.sh
+++ clearscope_e3.sh
@@ -1,100 +1,94 @@
 #!/bin/bash
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate orthrus
 
-# 定义参数数组
-learning_rates=(0.0015)  # lr 参数的不同取值
-lr_fs=(0.0015)   # lr 参数的不同取值
-mask_rates=(0.1 0.3 0.5)        # mask_rate 参数的不同取值
-layers=(2)                          # layer 参数的不同取值
-seeds=(1)                                 # seeds 参数的不同取值
-epochs=(5 50 200)                                # 训练 epochs
-num_hiddens=(128)                # num_hidden 参数的不同取值
-replace_rates=(0.0)                    # num_decoder_layers 参数的不同取值
-weight_decays=(0.01 0.001 0.0001)  # weight_decay 参数的不同取值
-weight_decay_fs=(5e-5 2e-6)  # weight_decay_f 参数的不同取值
-num_heads=(2 4 8)                         # num_heads 参数的不同取值
-max_epoch_fs=(50 100 200)               # max_epoch_f 参数的不同取值
-drop_edge_rates=(0.0)    # drop_edge_rate 参数的不同取值
-in_drops=(0.2)                    # in_drop 参数的不同取值
-attn_drops=(0.1)                  # attn_drop 参数的不同取值
-alpha_ls=(3)                  # alpha_l 参数的不同取值
+learning_rates=(0.0015 0.00015 0.0001)
+lr_fs=(0.001)
+mask_rates=(0.1 0.3 0.5)
+layers=(2)
+seeds=(1)
+epochs=(5 50 200)
+num_hiddens=(64 128 256)
+replace_rates=(0.0)
+weight_decays=(0.01 1e-3 1e-4 1e-5)
+num_heads=(2 4 8)
+max_epoch_fs=(50)
+drop_edge_rates=(0.0)
+in_drops=(0.2)
+attn_drops=(0.1)
+alpha_ls=(3)
 dataset="CLEARSCOPE_E3"
-data_path="clearscope_e3.pt"
+data_path="clearscope_e3_merge_edge_data.pt"
 ground_truth_path="../Ground_Truth/ground_truth_nids_clearscope.pt"
-gpu_ids=(1 1 1)
 counter=0
-# 循环遍历所有参数组合
+
 for seed in "${seeds[@]}"
 do
-  for max_epoch_f in "${max_epoch_fs[@]}"
+  for epoch in "${epochs[@]}"
   do
-    for epoch in "${epochs[@]}"
+    for layer in "${layers[@]}"
     do
-      for layer in "${layers[@]}"
+      for lr_f in "${lr_fs[@]}"
       do
-        for lr_f in "${lr_fs[@]}"
+        for lr in "${learning_rates[@]}"
         do
-          for lr in "${learning_rates[@]}"
+          for mask_rate in "${mask_rates[@]}"
           do
-            for mask_rate in "${mask_rates[@]}"
+            for replace_rate in "${replace_rates[@]}"
             do
-              for replace_rate in "${replace_rates[@]}"
+              for num_hidden in "${num_hiddens[@]}"
               do
-                for num_hidden in "${num_hiddens[@]}"
+                for weight_decay in "${weight_decays[@]}"
                 do
-                  for weight_decay in "${weight_decays[@]}"
+                  for num_head in "${num_heads[@]}"
                   do
-                    for weight_decay_f in "${weight_decay_fs[@]}"
+                    for max_epoch_f in "${max_epoch_fs[@]}"
                     do
-                      for num_head in "${num_heads[@]}"
+                      for drop_edge_rate in "${drop_edge_rates[@]}"
                       do
-                        for drop_edge_rate in "${drop_edge_rates[@]}"
+                        for in_drop in "${in_drops[@]}"
                         do
-                          for in_drop in "${in_drops[@]}"
+                          for attn_drop in "${attn_drops[@]}"
                           do
-                            for attn_drop in "${attn_drops[@]}"
+                            for alpha_l in "${alpha_ls[@]}"
                             do
-                              for alpha_l in "${alpha_ls[@]}"
-                              do
-                                device=${gpu_ids[$counter % 3]}
+                              CUDA_VISIBLE_DEVICES=1 python main_transductive.py \
+                                --device 0 \
+                                --dataset $dataset \
+                                --mask_rate $mask_rate \
+                                --encoder "gatedge" \
+                                --decoder "gat" \
+                                --in_drop $in_drop \
+                                --attn_drop $attn_drop \
+                                --num_layers $layer \
+                                --num_hidden $num_hidden \
+                                --num_heads $num_head \
+                                --max_epoch $epoch \
+                                --max_epoch_f $max_epoch_f \
+                                --lr $lr \
+                                --weight_decay $weight_decay \
+                                --lr_f $lr_f \
+                                --weight_decay_f 1e-4 \
+                                --activation prelu \
+                                --optimizer adam \
+                                --drop_edge_rate "$drop_edge_rate" \
+                                --loss_fn "sce" \
+                                --seeds $seed \
+                                --replace_rate $replace_rate \
+                                --alpha_l $alpha_l \
+                                --linear_prob \
+                                --scheduler \
+                                --use_cfg \
+                                --data_path $data_path \
+                                --ground_truth_path $ground_truth_path \
+                                --raw_data_dir ../raw_data \
+                                >> clearscope_e3_sweep.log 2>&1
 
-                                # 运行 Python 脚本，替换对应的参数
-                                CUDA_VISIBLE_DEVICES=$device python main_transductive.py \
-                                  --device 0 \
-                                  --dataset $dataset \
-                                  --mask_rate $mask_rate \
-                                  --encoder "gatedge" \
-                                  --decoder "gat" \
-                                  --in_drop $in_drop \
-                                  --attn_drop $attn_drop \
-                                  --num_layers $layer \
-                                  --num_hidden $num_hidden \
-                                  --num_heads $num_head \
-                                  --max_epoch $epoch \
-                                  --max_epoch_f $max_epoch_f \
-                                  --lr $lr \
-                                  --weight_decay $weight_decay \
-                                  --lr_f $lr_f \
-                                  --weight_decay_f $weight_decay_f \
-                                  --activation prelu \
-                                  --optimizer adam \
-                                  --drop_edge_rate "$drop_edge_rate" \
-                                  --loss_fn "sce" \
-                                  --seeds $seed \
-                                  --replace_rate $replace_rate \
-                                  --alpha_l $alpha_l \
-                                  --linear_prob \
-                                  --scheduler \
-                                  --use_cfg \
-                                  --data_path $data_path \
-                                  --ground_truth_path $ground_truth_path \
-                                  --raw_data_dir ../raw_data \
-                                  >> clearscope_$device.log & \
+                              ((counter++))
+                              if [ $((counter % 1)) -eq 0 ]; then
+                                wait
+                              fi
 
-                                ((counter++))
-                                if [ $((counter % 3)) -eq 0 ]; then
-                                  wait
-                                fi
-                              done
                             done
                           done
                         done
